7ef7f21de14ea88c9c6a53df5457877ab8f8fd98
Jasmine Cha <chajasmine@google.com>
2023-03-03 07:51:32 +0100

    a14xm: add audio hal hidl service
    - build audiohal hidl and needed libs
    - audio device sepolicy adapted from 'gs-common'
    
    Bug: 259161622
    Test: build pass and check hidl/aidl on gs201
          build pass for other chipset
    
    Change-Id: Id3c0446be6cdcd5a6e75c832e7d7f4a64716f79c
    Signed-off-by: Jasmine Cha <chajasmine@google.com>
    Signed-off-by: ramenrolls <ramenrolls@proton.me>
    (cherry picked from commit e471ef920acd1fa5342956b8ba03528b94500b0a)

--- device.mk
+++ device.mk
@@ -15,6 +15,54 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Declare as non AB device
 $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
+## Audio HAL Server & Default Implementations
+PRODUCT_PACKAGES += \
+	android.hardware.audio.service \
+	android.hardware.audio@7.1-impl \
+	android.hardware.audio.effect@7.0-impl \
+	android.hardware.soundtrigger@2.3-impl \
+	android.hardware.bluetooth.audio-impl \
+
+## Audio HAL libraries
+PRODUCT_PACKAGES += \
+	audio.primary.$(TARGET_BOARD_PLATFORM) \
+	audio.r_submix.$(TARGET_BOARD_PLATFORM) \
+	libaudioeffectoffload \
+	libbundlewrapper \
+	libreverbwrapper \
+	libeffectproxy \
+	libvisualizer \
+	libdynproc \
+	libdownmix \
+	libldnhncr \
+	libswdap \
+	libmyspace \
+	libswspatializer \
+	audio.usb.default \
+	libplaybackrecorder \
+	audio.bluetooth.default \
+	sound_trigger.primary.default
+
+PRODUCT_PACKAGES += \
+    libaudiopreprocessing_mtk
+
+## Libraries for audio.primary
+PRODUCT_PACKAGES += \
+    libtinyalsa \
+    libaudioroute \
+    tinymix
+
+PRODUCT_PACKAGES += \
+    MtkInCallService
+
+## AudioHAL Configurations
+PRODUCT_COPY_FILES += \
+	frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration_7_0.xml \
+	frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration_7_0.xml \
+	frameworks/av/services/audiopolicy/config/hearing_aid_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/hearing_aid_audio_policy_configuration_7_0.xml \
+	frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
+	frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
+
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
