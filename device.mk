3f4b1b81bc09e1dde7aeb7c7538b8ecab5eeae16
ramenrolls <ramenrolls@proton.me>
2024-07-06 09:02:01 +0200

    a14xm: "Android Error Exception" is a vendor-specific service (MTK) that hooks into the kernel to collect system errors, we don't need it. Other means of error collection and logs are available

--- device.mk
+++ device.mk
@@ -415,7 +415,6 @@ PRODUCT_PACKAGES += \
     factory_init.connectivity.rc \
     factory_init.project.rc \
     factory_init.rc \
-    init.aee.rc \
     init.ago.rc \
     init.connectivity.common.rc \
     init.connectivity.rc \
