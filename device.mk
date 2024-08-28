9b0f19fabb3b08d3bcae8de11cd2b308a9d1e705
Daniel Rosenberg <drosen@google.com>
2024-08-28 18:58:20 +0100

    a14xm: Enable Project ID Quota support on userdata
     * These features enable wide inodes on userdata, which
       is required for project ID quotas. Wide inodes can
       only be set at MKFS time.
    
     * This also enables casefolding, which is required for the
       removal of SDCardFS.
    
    Bug: 138321217
    Test: Examine filesystem features for project quota and wide inodes.
    Change-Id: I9c17cda2af34c6fbd73c2d90eb6b465f4939df67
    (cherry picked from commit 1df9c1dd4ee98a780bcad1dfd124564bd1118de8)
    (cherry picked from commit 9efde6c3236d8c6c61af0d5a2d00ac7b7abdf3f8)

--- device.mk
+++ device.mk
@@ -6,6 +6,9 @@
 # Dalvik VM Configuration
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
+# Project ID Quota
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
